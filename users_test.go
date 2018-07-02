90668e5c99f589e659ac786423a0256c43d4b0a9
delphinus <delphinus@remora.cx>
2018-07-02 14:17:28 +0900

    Add test for Env

--- users_test.go
+++ users_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -79,6 +80,80 @@ func TestOriginRemoteURL(t *testing.T) {
 			}
 			a.NoError(err)
 			a.Equal(c.expected, string(url))
+		})
+	}
+}
+
+func TestEnv(t *testing.T) {
+	a := assert.New(t)
+	execCommand = func(_ string, _ ...string) outputter {
+		return testOutputter{}
+	}
+	re, err := regexp.Compile(`ghe\.example\.com`)
+	a.NoError(err)
+	us := Users{
+		{
+			Name:      []byte(`Foo Bar`),
+			Email:     []byte(`foo@example.com`),
+			URLRegexp: re,
+		},
+	}
+	for _, c := range []struct {
+		name        string
+		outBytes    []byte
+		outErr      error
+		expected    []string
+		expectedErr string
+	}{
+		{
+			name:        "if originRemoteURL returns error, returns error",
+			outErr:      errors.New("hoge"),
+			expectedErr: "hoge",
+		},
+		{
+			name: "if url not match, returns error",
+			outBytes: []byte(`* remote origin
+  Fetch URL: https://hoge.hogeo.com
+  Push  URL: https://hoge.hogeo.com
+  HEAD branch: (not queried)
+  Local ref configured for 'git push' (status not queried):
+    (matching) pushes to (matching)
+`),
+			expectedErr: ErrNotMatch{[]byte(`hoge.hogeo.com`)}.Error(),
+		},
+		{
+			name: "if url match, returns valid env",
+			outBytes: []byte(`* remote origin
+  Fetch URL: https://ghe.example.com/hoge/fuga
+  Push  URL: ssh://git@ghe.example.com/hoge/fuga
+  HEAD branch: (not queried)
+  Remote branches: (status not queried)
+    development
+    master
+  Local branches configured for 'git pull':
+    development merges with remote development
+    master      merges with remote master
+  Local ref configured for 'git push' (status not queried):
+    (matching) pushes to (matching)
+`),
+			expected: []string{
+				"GIT_COMMITTER_NAME=Foo Bar",
+				"GIT_COMMITTER_EMAIL=foo@example.com",
+				"GIT_AUTHOR_NAME=Foo Bar",
+				"GIT_AUTHOR_EMAIL=foo@example.com",
+				"HUB_CONFIG=",
+			},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			outBytes = c.outBytes
+			outErr = c.outErr
+			env, err := us.Env()
+			if c.expectedErr != "" {
+				a.EqualError(err, c.expectedErr)
+				return
+			}
+			a.Equal(c.expected, env)
 		})
 	}
 }
